0d11877a20cef1db8731cbf1f3e59d0516bbb083
KarthikC-777 <karthik.c@ymedialabs.com>
2023-01-18 16:48:28 +0530

    added-hbs

--- src/auto-data/auto-data.controller.ts
+++ src/auto-data/auto-data.controller.ts
@@ -1,12 +1,22 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Render, Res } from '@nestjs/common';
 import { AutoDataService } from './auto-data.service';
-
 @Controller('vehicle-info')
 export class AutoDataController {
   constructor(private autodtaService: AutoDataService) {}
 
   @Get('vin/:vin')
+  @Render('index')
   async getVehicleDetailsByVin(@Param('vin') vin: string) {
-    return await this.autodtaService.getVehicleDetailsByVin(vin);
+    const result = await this.autodtaService.getVehicleDetailsByVin(vin);
+    return {
+      vin: result.vin,
+      year: result.year,
+      make: result.make,
+      model: result.model,
+      trim: result.trim,
+      color: result.color,
+      colorHex: result.colorHex,
+      styleId: result.styleId,
+    };
   }
 }
